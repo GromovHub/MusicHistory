f2357b2c8f044002e55b3ad5f8ebca46dc5e7e3e
gromov <gromovitaly@gmail.com>
2022-06-16 11:20:04 +0300

    init

---
+++ 1001 Albums/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  1001 Albums
+//
+//  Created by Vitaly Gromov on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.small)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
