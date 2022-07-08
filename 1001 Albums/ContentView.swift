c5a3a860c0a00bb9d320b3fc45b76f77dd0b29f9
Vitaly Gromov <gromovitaly@gmail.com>
2022-07-09 01:21:38 +0300

    ui test

--- 1001 Albums/ContentView.swift
+++ 1001 Albums/ContentView.swift
@@ -7,16 +7,51 @@
 
 import SwiftUI
 
+class FMViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    var imageName = "dog"
+    init() {
+        getImageFromAssets()
+    }
+    func getImageFromAssets() {
+        image = UIImage(named: imageName)
+    }
+    func changeName(name: String) {
+        imageName = name
+    }
+}
+
 struct ContentView: View {
     
-    var controller = Controller()
+    @StateObject var controller = Controller()
+    @StateObject var vm = FMViewModel()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.small)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 300, height: 400)
+                        .clipped()
+                        .cornerRadius(20)
+                }
+                Button {
+                    //button code
+                    vm.changeName(name: "cat")
+                    vm.getImageFromAssets()
+                } label: {
+                    Text("Save to FM")
+                        .foregroundColor(.white)
+                        
+                        .font(.headline)
+                        .padding()
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }.navigationTitle(Text("File Manager"))
         }
     }
 }
--- 1001 Albums/Controller.swift
+++ 1001 Albums/Controller.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Controller {
+class Controller: ObservableObject {
     init() {
         createArray()
     }
