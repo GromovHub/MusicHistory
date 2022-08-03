91b1fd2de21e093d2e998e3a73f4470d7171f7b3
Vitaly Gromov <gromovitaly@gmail.com>
2022-08-03 09:24:27 +0300

    swiftui combine

--- 1001 Albums/Garbage/TestPublisher.swift
+++ 1001 Albums/Garbage/TestPublisher.swift
@@ -13,19 +13,64 @@ class TestPublisherViewModel: ObservableObject {
         numberPublisher
             .receive(on: RunLoop.main)
 //            .assign(to: \.customColor, on: self)
-            .sink(receiveValue: { myColor in
+            .sink(receiveValue: { [weak self] myColor in
+                guard let self = self else { return }
                 self.customColor = myColor
             })
             .store(in: &cancel)
+        printNumber()
+//        numberPublisherFunc()
         
     }
+    
     @Published var number = ""
+        
     @Published var customColor = Color.blue
     var cancel = Set<AnyCancellable>()
+    var flag = true.description
+    
+    func printNumber() {
+        $number
+            .receive(on: RunLoop.main)
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .sink { i in
+                print(i)
+                if let number = Int(self.number), number > 100 {
+                    for ii in self.cancel {
+                        self.cancel.first?.cancel()
+                        print("subscriber is dead", ii)
+                    }
+                    
+                }
+            }
+            .store(in: &cancel)
+    }
+    
+    func numberPublisherFunc() {
+        $number
+            .receive(on: RunLoop.main)
+            .debounce(for: 1.5, scheduler: RunLoop.main)
+            .dropFirst()
+            .map {
+                guard let number = Int($0) else {
+                    return Color.yellow
+                }
+                if number > 5 {
+                    return Color.green
+                } else {
+                    return Color.red
+                }
+                 }
+            .sink(receiveValue: {myColor in
+                self.customColor = myColor
+            })
+            .store(in: &cancel)
+    }
     
     var numberPublisher: AnyPublisher<Color, Never> {
         $number
             .receive(on: RunLoop.main)
+            .debounce(for: 0.5, scheduler: RunLoop.main)
             .dropFirst()
             .map {
                 guard let number = Int($0) else {
@@ -39,6 +84,8 @@ class TestPublisherViewModel: ObservableObject {
                  }
             .eraseToAnyPublisher()
     }
+    
+   
     
 }
 
